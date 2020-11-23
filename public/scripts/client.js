c3ca316bcfdee51ac0dca67fa3aff51e3eecf3d3
salar taeb <salartaeb10@gmail.com>
2020-11-23 02:52:28 +0000

    added submit handler for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,47 +11,70 @@ const tweetData = [ {
   "created_at": 1461116232227
 }]
 
+
+
 $(document).ready(function() { 
 
-
+// RENDER TWEETS FROM DATABASE
 function renderTweets(tweets) {
   for (let i = 0, len = tweets.length; i < len; i++) {
     console.log(createTweetElement(tweets[i]))
     $('.tweet-container').append(createTweetElement(tweets[i]));
   }
- 
 };
 
-  
-  function createTweetElement(data) {
-    const $tweet = $("<article/>");
-  
-    // header element
-    const $header = $(
-      `<header>
-      <img class="avatar" src="${data.user.avatars}" alt="users avatar">
-      <h2>${data.user.name}</h2>
-      <p class="username">${data.user.handle}</p>
-      <p class="tweet-text">${data.content.text}</p>
-    </header>`
-    )
-  
-    const $footer = $(
-      `<footer>
-      <p></p>
-      <p>
-      <span><i class="fas fa-flag fa-1x"></i></span>
-      <span><i class="fas fa-heart fa-1x"></i></span>
-      <span><i class="fas fa-retweet fa-1x"></i></i></span>
-    </p>
-      `
-    )
+ 
+function createTweetElement(data) {
+  const $tweet = $("<article/>");
+  // header element
+  const $header = $(
+    `
+    <header>
+    <img class="avatar" src="${data.user.avatars}" alt="users avatar">
+    <h2>${data.user.name}</h2>
+    <p class="username">${data.user.handle}</p>
+    <p class="tweet-text">${data.content.text}</p>
+  </header>
+  `
+  )
+  const $footer = $(
+    `<footer>
+    <p></p>
+    <p>
+    <span><i class="fas fa-flag fa-1x"></i></span>
+    <span><i class="fas fa-heart fa-1x"></i></span>
+    <span><i class="fas fa-retweet fa-1x"></i></i></span>
+  </p>
+    `
+  )
+  return $tweet.append($header).append($footer);
+}
 
-    return $tweet.append($header).append($footer);
-  }
-  
-  renderTweets(tweetData);
-  
+renderTweets(tweetData);
+
+
+const URL = 'http://localhost:8080';
+
+  $(function() {
+    var $submit = $(".new-tweet");
+    $submit.on('submit', function (event) {
+      console.log('Button clicked, performing ajax call...');
+      event.preventDefault();
+      var formInput = $(this).serialize();
+      console.log($(this).serialize());
+      $.ajax({
+        url: `${URL}/tweets/`,
+        method: 'POST',
+        data: formInput,
+        success: function (data) {
+          console.log('Success: ', data);
+          $('#tweet-text').val('');
+          renderTweets(data);
+        }
+      });
+    });
+  });
+
 
 });
 
